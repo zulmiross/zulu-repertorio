a52c14d244dddafe8e1e86807d800ab8ea0f91d9
zulmiross <zulejesus@hotmail.com>
2024-05-16 06:54:50 -0300

    update

---
+++ www/js/menu.js
@@ -0,0 +1,1 @@
+$(".framework7-root").css('background', 'black');
--- www/js/routes.js
+++ www/js/routes.js
@@ -1,116 +1,131 @@
 //INICIALIZAÇÃO DO F7 QUANDO DISPOSITIVO ESTÁ PRONTO
 document.addEventListener('deviceready', onDeviceReady, false);
 var app = new Framework7({
-  // App root element
-  el: '#app',
-  // App Name
-  name: 'My App',
-  // App id
-  id: 'com.myapp.test',
-  // Enable swipe panel
-  panel: {
-    swipe: true,
-  },
-  dialog: {
-    buttonOk: 'Sim',
-    buttonCancel: 'Cancelar',
-  },
-  // Add default routes
-  routes: [
-    {
-      path: '/index/',
-      url: 'index.html',
-      animate: false,
-	  on: {
-		pageBeforeIn: function (event, page) {
-		// fazer algo antes da página ser exibida
-		},
-		pageAfterIn: function (event, page) {
-		// fazer algo depois da página ser exibida
-		},
-		pageInit: function (event, page) {
-		// fazer algo quando a página for inicializada
-		},
-		pageBeforeRemove: function (event, page) {
-		// fazer algo antes da página ser removida do DOM
-		},
-	  }
+    // App root element
+    el: '#app',
+    // App Name
+    name: 'My App',
+    // App id
+    id: 'com.myapp.test',
+    // Enable swipe panel
+    panel: {
+        swipe: true,
     },
-    {
-      path: '/link2/',
-      url: 'link2.html',
-      animate: false,
-	  on: {
-		pageBeforeIn: function (event, page) {
-		// fazer algo antes da página ser exibida
-		},
-		pageAfterIn: function (event, page) {
-		// fazer algo depois da página ser exibida
-		},
-		pageInit: function (event, page) {
-		// fazer algo quando a página for inicializada
-		},
-		pageBeforeRemove: function (event, page) {
-		// fazer algo antes da página ser removida do DOM
-		},
-	  }
+    dialog: {
+        buttonOk: 'Sim',
+        buttonCancel: 'Cancelar',
     },
-    {
-      path: '/link3/',
-      url: 'link3.html',
-      animate: false,
-	  on: {
-		pageBeforeIn: function (event, page) {
-		// fazer algo antes da página ser exibida
-		},
-		pageAfterIn: function (event, page) {
-		// fazer algo depois da página ser exibida
-		},
-		pageInit: function (event, page) {
-		// fazer algo quando a página for inicializada
-		},
-		pageBeforeRemove: function (event, page) {
-		// fazer algo antes da página ser removida do DOM
-		},
-	  }
-    },
-  ],
-  // ... other parameters
+    // Add default routes
+    routes: [{
+            path: '/index/',
+            url: 'index.html',
+            // options: {
+            //     transition: 'f7-push'
+            // },
+            animate: false,
+            on: {
+                pageBeforeIn: function(event, page) {
+                    // fazer algo antes da página ser exibida
+                    $(".toolbar").show('fast');
+
+                },
+                pageAfterIn: function(event, page) {
+                    // fazer algo depois da página ser exibida
+                },
+                pageInit: function(event, page) {
+                    // fazer algo quando a página for inicializada
+                    // $.getScript('js/index.js')
+                    // app.views.main.router.navigate("/menu/");
+
+                },
+                pageBeforeRemove: function(event, page) {
+                    // fazer algo antes da página ser removida do DOM
+                },
+            }
+        },
+        {
+            path: '/menu/',
+            url: 'menu.html',
+            options: {
+                transition: 'f7-cover-v'
+            },
+            animate: false,
+            on: {
+                pageBeforeIn: function(event, page) {
+                    // fazer algo antes da página ser exibida
+                    $(".toolbar").hide();
+
+                },
+                pageAfterIn: function(event, page) {
+                    // fazer algo depois da página ser exibida
+                },
+                pageInit: function(event, page) {
+                    // fazer algo quando a página for inicializada  
+                    $.getScript('js/menu.js');
+                },
+                pageBeforeRemove: function(event, page) {
+                    // fazer algo antes da página ser removida do DOM
+                },
+            }
+        },
+        {
+            path: '/perfil/',
+            url: 'perfil.html',
+            animate: false,
+            on: {
+                pageBeforeIn: function(event, page) {
+                    // fazer algo antes da página ser exibida
+
+                },
+                pageAfterIn: function(event, page) {
+                    // fazer algo depois da página ser exibida
+                },
+                pageInit: function(event, page) {
+                    // fazer algo quando a página for inicializada                   
+                },
+                pageBeforeRemove: function(event, page) {
+                    // fazer algo antes da página ser removida do DOM
+                },
+            }
+        },
+
+    ],
+    // ... other parameters
 });
 
 //Para testes direto no navegador
-//var mainView = app.views.create('.view-main', { url: '/index/' });
+var mainView = app.views.create('.view-main', { url: '/index/' });
 
 //EVENTO PARA SABER O ITEM DO MENU ATUAL
-app.on('routeChange', function (route) {
-  var currentRoute = route.url;
-  console.log(currentRoute);
-  document.querySelectorAll('.tab-link').forEach(function (el) {
-    el.classList.remove('active');
-  });
-  var targetEl = document.querySelector('.tab-link[href="' + currentRoute + '"]');
-  if (targetEl) {
-    targetEl.classList.add('active');
-  }
+app.on('routeChange', function(route) {
+    var currentRoute = route.url;
+    console.log(currentRoute);
+    document.querySelectorAll('.tab-link').forEach(function(el) {
+        el.classList.remove('active');
+    });
+    var targetEl = document.querySelector('.tab-link[href="' + currentRoute + '"]');
+    if (targetEl) {
+        targetEl.classList.add('active');
+    }
 });
 
 
 function onDeviceReady() {
-  //Quando estiver rodando no celular
-  var mainView = app.views.create('.view-main', { url: '/index/' });
+    //Quando estiver rodando no celular
+    // var mainView = app.views.create('.view-main', { url: '/index/' });
 
-  //COMANDO PARA "OUVIR" O BOTAO VOLTAR NATIVO DO ANDROID 	
-  document.addEventListener("backbutton", function (e) {
+    //COMANDO PARA "OUVIR" O BOTAO VOLTAR NATIVO DO ANDROID 	
+    document.addEventListener("backbutton", function(e) {
 
-    if (mainView.router.currentRoute.path === '/index/') {
-      e.preventDefault();
-      app.dialog.confirm('Deseja sair do aplicativo?', function () {
-        navigator.app.exitApp();
-      });
-    } else {
-      e.preventDefault();
-      mainView.router.back({ force: true });
-    }
-  }, false);
+        if (mainView.router.currentRoute.path === '/index/') {
+            e.preventDefault();
+            app.dialog.confirm('Deseja sair do aplicativo?', function() {
+                navigator.app.exitApp();
+            });
+        } else {
+            e.preventDefault();
+            mainView.router.back({ force: true });
+        }
+    }, false);
 
 }
